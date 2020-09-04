f8a2fac99fc3663c532502719c99d05822695a40
Will Pope <wpope@uw.edu>
2020-09-04 15:41:30 -0700

    updating RWA script

--- rwa/rwaTesting/master_RPi.py
+++ rwa/rwaTesting/master_RPi.py
@@ -243,12 +243,16 @@ def spiTransfer(reqArr1,rplN1):
 
     reqArrH = flatList([0x7e, reqArr1, 0x7e]) 
     reqArrX = xorSwitch(reqArrH, "reqMode")               
- 
+    
+    print('reqArrX: ', reqArrX)
     slvEmpArr = spi.xfer2(reqArrX)
+    print('slvEmpArr: ', slvEmpArr)
 
     time.sleep(0.100)                           # waits 100 ms for RWA to process
-       
+    
+    print('msrEmpArr: ', msrEmpArr)   
     rplArrX = spi.xfer2(msrEmpArr)
+    print('rplArrX: ', rplArrX)
 
     rplArrH = xorSwitch(rplArrX, "rplMode")   
     rplArr1 = rplArrH[(0+2):(rplN1+2)] 
