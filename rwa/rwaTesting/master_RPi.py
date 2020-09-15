612ebf9b59c845e83f6502771ec8f33b39d53dba
Will Pope <wpope@uw.edu>
2020-09-15 15:58:01 -0700

    updating RWA scripts

--- rwa/rwaTesting/master_RPi.py
+++ rwa/rwaTesting/master_RPi.py
@@ -260,7 +260,7 @@ def spiTransfer(reqArr1,rplN1):
     GPIO.output(21, True)
     slvEmpArr = list(spiRx)
     
-    time.sleep(0.200)                                   # try decreasing wait time 
+    time.sleep(0.200)                                   
     
     spiTx = msrEmpArr
     GPIO.output(21, False)
