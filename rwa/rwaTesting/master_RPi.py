1dd9215f15025602682c9a66e05d2e5393d47399
Will Pope <wpope@uw.edu>
2020-09-15 00:53:31 -0700

    updating RWA scripts

--- rwa/rwaTesting/master_RPi.py
+++ rwa/rwaTesting/master_RPi.py
@@ -1242,11 +1242,10 @@ while True:
                 txByteArray1[c] = inpCharList[2*c] + inpCharList[(2*c)+1]
                 txByteArray1[c] = int(txByteArray1[c],16) 
 
-            #rplN2 = int(input("enter expected reply length (bytes): \n"))
+            txByteArray1 = flatList([0x7e, txByteArray1, 0x7e])  
 
-            
-            
-            txByteArray1 = flatList([0x7e, txByteArray1, 0x7e])               
+            rplN2 = int(input("enter expected reply length (bytes): \n")) 
+            txByteArray2 = [0x7e] * (2*rplN2 + 3)             
          
             GPIO.output(21, False)
             spiTx = list(txByteArray1)
@@ -1254,14 +1253,18 @@ while True:
             rxByteArray1 = list(spiRx)
             GPIO.output(21, True)
 
-            #time.sleep(0.200)                           
-            
-            #txByteArray2 = [0x7e] * (2*rplN2 + 3)    
-            #rxByteArray2 = spi.xfer2(txByteArray2)
+            time.sleep(0.200)                           
+
+            GPIO.output(21, False)
+            spiTx = list(txByteArray2)
+            spiRx = spi.xfer2(spiTx)
+            rxByteArray2 = list(spiRx)
+            GPIO.output(21, True)
+
             print('txByteArray1: ', [hex(x) for x in txByteArray1])
             print('rxByteArray1: ', [hex(x) for x in rxByteArray1])
-            #print('txByteArray2: ', [hex(x) for x in txByteArray2])
-            #print('rxByteArray2: ', [hex(x) for x in rxByteArray2])
+            print('txByteArray2: ', [hex(x) for x in txByteArray2])
+            print('rxByteArray2: ', [hex(x) for x in rxByteArray2])
             print(" ")
 
             
