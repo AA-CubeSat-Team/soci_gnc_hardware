1b1f62d972c88d0cb8fab597c7d038cffab92339
chojon <chojon@uw.edu>
2020-11-30 18:08:01 -0800

    modified slave code for SDA1/SCL1

--- sensor-spoof/Templates/example_slave/example_slave.ino
+++ sensor-spoof/Templates/example_slave/example_slave.ino
@@ -1,23 +1,25 @@
 #include <Wire.h>
 #define SLAVE_ADDR 4
 #define ANSWERSIZE 2
+extern TwoWire Wire1;
 
 String answer = "Wo";
 void setup() {
   // put your setup code here, to run once:
-  Wire.begin(SLAVE_ADDR); //Runs in SLAVE mode
-  Wire.onRequest(requestEvent); 
-  Wire.onReceive(receiveEvent);
+  Wire1.begin(); //Runs in SLAVE mode
+  Wire1.onRequest(requestEvent); 
+  Wire1.onReceive(receiveEvent);
 
   Serial.begin(9600);
   Serial.println("I2C Test");
+
 }
 
 // This method takes bytes from the master
 void receiveEvent(int numBytes) { // from Arduino Master 1
   String response = "";
-  while(Wire.available()) {
-    char b = Wire.read();
+  while(Wire1.available()) {
+    char b = Wire1.read();
     response += b;
   }
     Serial.println(response);
@@ -30,7 +32,7 @@ void requestEvent(){ // from Arduino Master 2
   for(byte i=0;i<ANSWERSIZE;i++) {
     response[i] = (byte)answer.charAt(i);
   }
-  Wire.write(response,sizeof(response));
+  Wire1.write(response,sizeof(response));
 }
 
 void loop() {
