4c8ffb177971d7472c0ebc5d00d6ac5bb672a99b
Raiham Malik <raiham@uw.edu>
2021-02-28 18:40:29 -0800

    master code added for other teams

---
+++ sensor-spoof/Slave_Sensors/Master_Code/Master_Code.ino
@@ -0,0 +1,33 @@
+// Master Code Simulink
+
+#include <Wire.h>
+#define SLAVE_ADDR1 4
+#define SLAVE_ADDR2 5
+#define ANSWERSIZE num
+
+void setup()
+{
+  //Wire.begin(); // join i2c bus (address optional for master)
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int response; //integer passed from masterleft
+  Wire.begin();
+  delay(1000);
+  Wire.requestFrom(SLAVE_ADDR1, 1); //pulls data from available wire from slave
+  response = Wire.read();
+  Serial.print("Value from Slave 1: ");
+  Serial.println(response);
+  Wire.endTransmission();
+  
+  delay(1000);
+  Wire.begin();
+  Wire.requestFrom(SLAVE_ADDR2, 1); //pulls data from available wire from slave
+  response = Wire.read();
+  Serial.print("Value from Slave 2: ");
+  Serial.println(response);
+  Wire.endTransmission();
+
+}
