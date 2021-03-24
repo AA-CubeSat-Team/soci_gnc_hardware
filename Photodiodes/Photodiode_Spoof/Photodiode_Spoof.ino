cdf36ae1a29a39bba6bc0413194b310c270331ee
Will Pope <wpope@uw.edu>
2021-03-24 00:08:18 -0700

    updated photodiode spoof with my changes (Will)

--- Photodiodes/Photodiode_Spoof/Photodiode_Spoof.ino
+++ Photodiodes/Photodiode_Spoof/Photodiode_Spoof.ino
@@ -1,51 +1,138 @@
+// photodiode spoof
+
+#include <Wire.h>
 
 // Givens
-double R = 10000;
-double V_ref = 3.3;
+#define R 10000
+#define V_ref 3.3
+#define ADC_ser_address 0x1D // ADC address
+
+#define PD1_reg 0x20
+#define PD2_reg 0x21
+#define PD3_reg 0x22
+#define PD4_reg 0x23
+#define PD5_reg 0x24
 
 // Function Variables 
 double PD_comp;           //array of inputs
 uint16_t D_out_12bit[5];  // Array of spoof outputs
+uint8_t req_PD_reg;
   
-// Testing function
-double test_one = .000001;
-double test_two = .000002;
-double test_three = .000003;
-double test_four = .000004;
-double test_five = .000005;
-  
+// Test values (replace with Simulink serial input later)
+double test_val_1 = .000001;
+double test_val_2 = .000002;
+double test_val_3 = .000003;
+double test_val_4 = .000004;
+double test_val_5 = .000005;
+double test_val_array[] = {test_val_1,test_val_2,test_val_3,test_val_4,test_val_5};
+
+// define D_out array as a global to be called by the requestEvent() function
+uint8_t D_out_byte_array[10] = {0};
+
+
 void setup(){
-  // Using PD spoof with arbitrary inputs
-  PD_spoof(test_one, test_two, test_three, test_four, test_five);
+  Serial.begin(9600);
+  Serial.println("function start");
 
-// Printing outputs
-Serial.begin (9600); //Declare baude rate
-Serial.println("Input of Photodiode Spoof");
-Serial.println(test_one);
-Serial.println("Output of Photodiode Spoof, double");
-Serial.println(D_out_12bit[0]);
-Serial.println(D_out_12bit[1]);
-Serial.println(D_out_12bit[2]);
-Serial.println(D_out_12bit[3]);
-Serial.println(D_out_12bit[4]);
-}
-void loop()
-{
-}
-
-// Photodiode spoof
-void PD_spoof(double pd1, double pd2, double pd3, double pd4, double pd5){
-  // Initialization
+  // this data processing code will be relocated to the Simulink serial input function, once written
+  int ii = 0;
+  double dVIN = 0;
+  double D_out_64bit = 0;
+  uint16_t D_out_16bit = 0;
   
-  // Compiling PD: 
-  double PD_comp[5] = {pd1, pd2, pd3, pd4, pd5};
-   // Calculating Digital signal: (page 28)
-  for (int i =0; i <5; i++) 
-  {
-    // Combination of Ohm's law for op amp ( Resistor = 10 kOhms ) and digital signal equation (page 28)
-    D_out_12bit[i] = int(PD_comp[i]*R/V_ref * pow(2,12)); 
+  for(ii=0; ii<5; ii++){
+    // split the calculation step into multiple lines for clarity
+    // D_out_16bit = desired 12bit value, but more clear that it still takes up 16 bits of storage
+    dVIN = test_val_array[ii]*R;
+    D_out_64bit = dVIN/V_ref * pow(2,12);
+    D_out_16bit = int(D_out_64bit);
 
+    // weird error where the script restarts at this point, can probably ignore it for now
+
+    // Wire.write() requires the data to be split into individual bytes, 
+    // so these two lines split each 16 bit D_out into two bytes
+    // bytes are arranged in little endian: [PD1_LSB, PD1_MSB, PD2_LSB, PD2_MSB, ...]
+    D_out_byte_array[2*ii] = D_out_16bit & 0xFF;
+    D_out_byte_array[2*ii+1] = (D_out_16bit >> 8) & 0xFF;
+  }
+  delay(1000);
+
+  // printing out D_out bytes for demonstration
+  Serial.print("PD1 D_out:\t");
+  Serial.print(D_out_byte_array[1],HEX);
+  Serial.println(D_out_byte_array[0],HEX);
+
+  Serial.print("PD2 D_out:\t");
+  Serial.print(D_out_byte_array[3],HEX);
+  Serial.println(D_out_byte_array[2],HEX);
+
+  Serial.print("PD3 D_out:\t");
+  Serial.print(D_out_byte_array[5],HEX);
+  Serial.println(D_out_byte_array[4],HEX);
+
+  Serial.print("PD4 D_out:\t");
+  Serial.print(D_out_byte_array[7],HEX);
+  Serial.println(D_out_byte_array[6],HEX);
+
+  Serial.print("PD5 D_out:\t");
+  Serial.print(D_out_byte_array[9],HEX);
+  Serial.println(D_out_byte_array[8],HEX);
+
+  Wire.begin(ADC_ser_address);      // join i2c bus with address 0x1D
+  Wire.onReceive(receiveEvent); // sets register event, calls function automatically when master sends data
+  Wire.onRequest(requestEvent); // sets request event, calls function automatically when master requests data
+}
+
+          
+void loop(){
+  // arbitrary, slave just waits for master to send/request data
+  delay(100);
+}
+
+
+void receiveEvent(int num_bytes){
+// this function is used when the master uses Wire.beginTransmission()->Wire.write()->Wire.endTransmission()
+  
+  // need to write code to process/sort values sent by master, this is a very rough draft
+  uint8_t received_array[3] = {0};    // creates array of three 0 that is reset every loop
+  int ii = 0;
+  while(Wire.available() > 0){ // loop through all but the last
+    received_array[ii] = Wire.read(); // receive byte as a character
+    ii++;
   }
 }
+
+
+void requestEvent(){
+// this function is used when the master uses Wire.requestFrom()
+
+  // need a higher level switch case to respond to all different requests
+
+    // beginning of channel voltage request code
+    // when master wants a channel voltage, it sends reg address then requests reg value (two transfers)
+    // request_reg is the register address sent by master in line 214 of Photodiode_Wrapper
+    // request_reg needs to be a global passed between written by receiveEvent() and used by requestEvent()
+    
+    switch (req_PD_reg){
+      case PD1_reg:
+        Wire.write(&D_out_byte_array[0],2);
+        break;
+      case PD2_reg:
+        Wire.write(&D_out_byte_array[2],2);
+        break;
+      case PD3_reg:
+        Wire.write(&D_out_byte_array[4],2);
+        break;
+      case PD4_reg:
+        Wire.write(&D_out_byte_array[6],2);
+        break;
+      case PD5_reg:
+        Wire.write(&D_out_byte_array[8],2);
+        break;
+    }
+}
+
+
+
 
   
--- Photodiodes/Photodiode_Wrapper/Photodiode_Wrapper.ino
+++ Photodiodes/Photodiode_Wrapper/Photodiode_Wrapper.ino
@@ -1,6 +1,14 @@
+// photodiode wrapper
+
 #include <Wire.h>
-uint8_t address = 0x1D; // ADC address
-uint16_t PDAdd[] = {0x20,0x21,0x22,0x23,0x24}; // Photodiode 1-5 register address'
+
+uint8_t ADC_ser_address = 0x1D; // ADC address
+#define PD1_reg 0x20
+#define PD2_reg 0x21
+#define PD3_reg 0x22
+#define PD4_reg 0x23
+#define PD5_reg 0x24
+uint8_t PD_reg[] = {PD1_reg,PD2_reg,PD3_reg,PD4_reg,PD5_reg}; // Photodiode 1-5 register address'
 uint16_t PDVol[10];
 uint16_t PDVoltage1;
 uint16_t PDVoltage2;
@@ -14,21 +22,21 @@ int j;
 
 //________________Quick Start (Page 33 in ADC Datasheet)_________________________
 // Step 3: Configuration 
-uint8_t advance_config_reg = 0x0B;
+#define advance_config_reg 0x0B
 uint8_t advance_config_value = 0b00000000;
 
 // Step 4: Enable Conversion Rate
-uint8_t start_reg = 0x00;
+#define start_reg 0x00
 uint8_t test;
-uint8_t conv_rate_reg = 0x07;
+#define conv_rate_reg 0x07
 uint8_t conv_rate_value = 0b0000001;
 
 // Step 5: Disable analog inputs (disables IN5,IN6,temp)
-uint8_t disable_reg = 0x08;
+#define disable_reg 0x08
 uint8_t disable_value = 0b11100000; 
 
 // Step 6: Enable interrupt mask 
-uint8_t interrupt_mask_reg = 0x03;
+#define interrupt_mask_reg 0x03
 uint8_t interrupt_mask_value = 0b00011111;
 
 //Step 7: Program limit registers
@@ -41,14 +49,14 @@ uint8_t in_low_reg[] = {0x2B,0x2D,0x2F,0x31,0x33};
 uint8_t in_low_val = 0;
 
 // Step 8: Starting the ADC
-uint8_t config_reg = 0x00;
+#define config_reg 0x00
 uint8_t config_default_value = 0b00001000;
 uint8_t start_value = 0b00001001;
 uint8_t c = 1;
-uint8_t busy_status_reg = 0x0C;
+#define busy_status_reg 0x0C
 
 // Step 9: Setting INT_clear to zero
-uint8_t int_clear_reg = 0x00;
+#define int_clear_reg 0x00
 uint8_t int_clear_val = 0b00001001;
 //______________________________________________________________________________________________
 
@@ -63,34 +71,48 @@ void setup(){
     }
   Wire.begin();
 
-  //_____________________ Quick Start Cont. _______________________________________________________
+  initADC();
+}
+
+
+
+void loop(){
+//  getVoltage();
+
+//  health();
+
+  delay(5000);
+}
+
+
+void initADC(){
   // STEP 1&2: wait for at least 33 ms
   while (c) {
-    readRegs(busy_status_reg, 1, &c);
+    readReg(busy_status_reg, 1, &c);
     }
     
   // STEP 3: Choosing internal VREF and mode of operation
   writeReg(advance_config_reg,advance_config_value);
-  readRegs(advance_config_reg,1, &advance_config_value);
+  readReg(advance_config_reg,1, &advance_config_value);
   Serial.println("Step3: operation mode");
   Serial.println(advance_config_value);
 
   // STEP 4: Enable Conversion Rate Register
   writeReg(conv_rate_reg,conv_rate_value);  
-  readRegs(conv_rate_reg,1,&conv_rate_value);
+  readReg(conv_rate_reg,1,&conv_rate_value);
   Serial.println("Step 4b: Conversion Rate value:");
   Serial.println(conv_rate_value);
   
   //STEP 5: Disabling the other seven inputs
   writeReg(disable_reg,disable_value);
-  readRegs(disable_reg,1,&disable_value);
+  readReg(disable_reg,1,&disable_value);
   writeReg(start_reg,0b00001001);  // Exit shutdown mode
   Serial.println("Step5b: Disabling inputs");
   Serial.println(disable_value,BIN);
 
   //STEP 6: Enabling interrupt mask (won't go to INT pin)
   writeReg(interrupt_mask_reg,interrupt_mask_value);
-  readRegs(interrupt_mask_reg,1,&interrupt_mask_value);
+  readReg(interrupt_mask_reg,1,&interrupt_mask_value);
   Serial.println("Enabling interrupt mask");
   Serial.println(interrupt_mask_value);
   
@@ -98,8 +120,8 @@ void setup(){
   for(i=0; i<sizeof(in_high_reg); i++){
     writeReg(in_high_reg[i],in_high_val);
     writeReg(in_low_reg[i],in_low_val);
-    readRegs(in_high_reg[i],1,&in_high_val);
-    readRegs(in_low_reg[i],1,&in_low_val);
+    readReg(in_high_reg[i],1,&in_high_val);
+    readReg(in_low_reg[i],1,&in_low_val);
     Serial.println("Step7: Limits for IN0-IN4");
     Serial.println(in_high_val);
     Serial.println(in_low_val);
@@ -107,28 +129,17 @@ void setup(){
 
   //STEP 8: Setting Start bit to 1   
   writeReg(config_reg, start_value);
-  readRegs(config_reg, 1, &c);
+  readReg(config_reg, 1, &c);
   Serial.println("Setting Start bit to 1");
   Serial.println(c,BIN);
 
   //STEP 9: Set interrupt to zero
   writeReg(int_clear_reg,int_clear_val);
-  readRegs(int_clear_reg,1,int_clear_val);
+  readReg(int_clear_reg,1,int_clear_val);
   Serial.println("Setting interrupt to zero");
   Serial.println(int_clear_val,BIN);
   Serial.println("------------------------------------------------------------------------------------");
-//___________________________________________________________________________________________________________
 }
-
-
-
-void loop(){
-  volt();
-
-  health();
-}
-
-
 
 // Health check: If any channel exceeds the maxV or all 5 channels exceed 3*maxV when summed: 1) resets registers to default values and puts ADC into shutdown mode. 2) Reinitializes ADC to proper configuration (same as quickstart procedure).
 void health()
@@ -138,31 +149,31 @@ void health()
     
       // STEP 1&2: wait for at least 33 ms
   while (c) {
-    readRegs(busy_status_reg, 1, &c);
+    readReg(busy_status_reg, 1, &c);
     }
     
   // STEP 3: Choosing internal VREF and mode of operation
   writeReg(advance_config_reg,advance_config_value);
-  readRegs(advance_config_reg,1, &advance_config_value);
+  readReg(advance_config_reg,1, &advance_config_value);
   Serial.println("Step3: operation mode");
   Serial.println(advance_config_value);
 
   // STEP 4: Enable Conversion Rate Register
   writeReg(conv_rate_reg,conv_rate_value);  
-  readRegs(conv_rate_reg,1,&conv_rate_value);
+  readReg(conv_rate_reg,1,&conv_rate_value);
   Serial.println("Step 4b: Conversion Rate value:");
   Serial.println(conv_rate_value);
   
   //STEP 5: Disabling the other seven inputs
   writeReg(disable_reg,disable_value);
-  readRegs(disable_reg,1,&disable_value);
+  readReg(disable_reg,1,&disable_value);
   writeReg(start_reg,0b00001001);  // Exit shutdown mode
   Serial.println("Step5b: Disabling inputs");
   Serial.println(disable_value,BIN);
 
   //STEP 6: Enabling interrupt mask (won't go to INT pin)
   writeReg(interrupt_mask_reg,interrupt_mask_value);
-  readRegs(interrupt_mask_reg,1,&interrupt_mask_value);
+  readReg(interrupt_mask_reg,1,&interrupt_mask_value);
   Serial.println("Enabling interrupt mask");
   Serial.println(interrupt_mask_value);
   
@@ -170,8 +181,8 @@ void health()
   for(i=0; i<sizeof(in_high_reg); i++){
     writeReg(in_high_reg[i],in_high_val);
     writeReg(in_low_reg[i],in_low_val);
-    readRegs(in_high_reg[i],1,&in_high_val);
-    readRegs(in_low_reg[i],1,&in_low_val);
+    readReg(in_high_reg[i],1,&in_high_val);
+    readReg(in_low_reg[i],1,&in_low_val);
     Serial.println("Step7: Limits for IN0-IN4");
     Serial.println(in_high_val);
     Serial.println(in_low_val);
@@ -179,13 +190,13 @@ void health()
 
   //STEP 8: Setting Start bit to 1   
   writeReg(config_reg, start_value);
-  readRegs(config_reg, 1, &c);
+  readReg(config_reg, 1, &c);
   Serial.println("Setting Start bit to 1");
   Serial.println(c,BIN);
 
   //STEP 9: Set interrupt to zero
   writeReg(int_clear_reg,int_clear_val);
-  readRegs(int_clear_reg,1,int_clear_val);
+  readReg(int_clear_reg,1,int_clear_val);
   Serial.println("Setting interrupt to zero");
   Serial.println(int_clear_val,BIN);
   Serial.println("------------------------------------------------------------------------------------");
@@ -195,15 +206,17 @@ void health()
 
 
 // Reads voltages from ADC
-void volt()
+void getVoltage()
 {
   j = 0;
-  for(i=0; i<sizeof(PDAdd); i++){ 
-    Wire.beginTransmission(address);
-    Wire.write(PDAdd[i]);     // Write register address
+  for(i=0; i<sizeof(PD_reg); i++){ 
+    Wire.beginTransmission(ADC_ser_address);
+    Wire.write(PD_reg[i]);     // Write register address
     Wire.endTransmission();
-    delay(80);    // Delay allowing for completion of conversion (~60ms for 5 voltage channels)
-    Wire.requestFrom(address,2);
+
+    // EDIT: delay needs to occur outside FOR loop: send 5 registers, wait, request 5 values
+    delay(80);    // Delay allowing for completion of conversion (~60ms for 5 voltage channels) 
+    Wire.requestFrom(ADC_ser_address,2);
     while(Wire.available()){
       PDVol[j] = Wire.read();     // Data byte 1
       PDVol[j+1] = Wire.read();     // Data byte 2
@@ -220,13 +233,13 @@ void volt()
 
 // Reads 'counts' number of values from registration
 // 'reg' and stores the values in 'dest[]'.
-void readRegs(uint8_t reg, uint8_t count, uint8_t dest[])
+void readReg(uint8_t reg, uint8_t count, uint8_t dest[])
 {
   i = 0;
-  Wire.beginTransmission(address);
+  Wire.beginTransmission(ADC_ser_address);
   Wire.write(reg);
   Wire.endTransmission(false);
-  Wire.requestFrom(address, count);
+  Wire.requestFrom(ADC_ser_address, count);
   while (Wire.available()) {
     dest[i++] = Wire.read();
   }
@@ -236,7 +249,7 @@ void readRegs(uint8_t reg, uint8_t count, uint8_t dest[])
 // writes registration 'reg' with value 'value'
 void writeReg(uint8_t reg, uint8_t value)
 {
-  Wire.beginTransmission(address);
+  Wire.beginTransmission(ADC_ser_address);
   Wire.write(reg);
   Wire.write(value);
   Wire.endTransmission();
