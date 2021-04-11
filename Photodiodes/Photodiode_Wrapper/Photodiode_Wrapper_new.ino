1ff9852f2a90cdceb3412a579ead49bdf8eb3a73
branton7 <65787522+branton7@users.noreply.github.com>
2021-04-10 19:57:41 -0700

    Branton: Some of Will's suggested edits made..
    More suggested edits still to be addressed

--- Photodiodes/Photodiode_Wrapper/Photodiode_Wrapper_new.ino
+++ Photodiodes/Photodiode_Wrapper/Photodiode_Wrapper_new.ino
@@ -8,7 +8,7 @@ uint8_t ADC_ser_address = 0x1D; // ADC address
 #define PD3_reg 0x22
 #define PD4_reg 0x23
 #define PD5_reg 0x24
-uint8_t PD_reg[] = {PD1_reg,PD2_reg,PD3_reg,PD4_reg,PD5_reg}; // Photodiode 1-5 register address'   //WILL: rename array to "PD_reg_list" or "PD_reg_array" for clarity
+uint8_t PD_reg_array[] = {PD1_reg,PD2_reg,PD3_reg,PD4_reg,PD5_reg}; // Photodiode 1-5 register address'
 uint16_t PDVol[10];
 uint16_t PDVoltage1;
 uint16_t PDVoltage2;
@@ -67,10 +67,24 @@ void setup(){
     {
     }
   Wire.begin();
+}
 
-  //_____________________ Quick Start Cont. _______________________________________________________
+
+void loop(){
+  runPHD();
+}
+
+
+
+void runPHD(){
+  getVoltage();
   
-// WILL: all of this quick start code (where you're actually reading/writing, line 71-165) should be placed in an initADC() function
+  health();
+}
+
+
+// Initialize ADC
+void initADC(){  
   // STEP 1&2: wait for at least 33 ms
   uint8_t c = 1;
   while (c) {                                 //WILL: is the 33 ms included anywhere here?
@@ -107,13 +121,14 @@ void setup(){
   Serial.println(interrupt_mask_value);
   
   //STEP7: Sending Limits for only enable input
-  writeReg(in_high_reg[0],in_high_val);               //WILL: break up this block of text for readability
+  writeReg(in_high_reg[0],in_high_val);
   writeReg(in_low_reg[0],in_low_val);
   readRegs(in_high_reg[0],1,&in_high_val);
   readRegs(in_low_reg[0],1,&in_low_val);
   Serial.println("Step7: Limits for IN0");
   Serial.println(in_high_val);
   Serial.println(in_low_val);
+  
   in_high_val = 0b101;
   in_low_val = 0;
   writeReg(in_high_reg[1],in_high_val);
@@ -123,6 +138,7 @@ void setup(){
   Serial.println("Step7: Limits for IN1");
   Serial.println(in_high_val);
   Serial.println(in_low_val);
+  
   in_high_val = 0b101;
   in_low_val = 0;
   writeReg(in_high_reg[2],in_high_val);
@@ -132,6 +148,7 @@ void setup(){
   Serial.println("Step7: Limits for IN2");
   Serial.println(in_high_val);
   Serial.println(in_low_val);
+  
   in_high_val = 0b101;
   in_low_val = 0;
   writeReg(in_high_reg[3],in_high_val);
@@ -141,6 +158,7 @@ void setup(){
   Serial.println("Step7: Limits for IN3");
   Serial.println(in_high_val);
   Serial.println(in_low_val);
+  
   in_high_val = 0b101;
   in_low_val = 0;
   writeReg(in_high_reg[4],in_high_val);
@@ -163,117 +181,15 @@ void setup(){
   Serial.println("Setting interrupt to zero");
   Serial.println(int_clear_val,BIN);
   Serial.println("------------------------------------------------------------------------------------");
-//___________________________________________________________________________________________________________
 }
-
-
-
-void loop(){    //WILL: add one higher layer of structure as a "runPHD()" function that contains getVoltage() and health(), this is what will run on the OBC
-  getVoltage();
-  
-  health();
-}
-
 
 
 // Health check: If any channel exceeds the maxV or all 5 channels exceed 3*maxV when summed: 1) resets registers to default values and puts ADC into shutdown mode. 2) Reinitializes ADC to proper configuration (same as quickstart procedure).
-void health()
-{
+void health() {
   if((PDVoltage1 > maxV || PDVoltage2 > maxV || PDVoltage3 > maxV || PDVoltage4 > maxV || PDVoltage5 > maxV) || PDVoltage1 + PDVoltage2 + PDVoltage3 + PDVoltage4 + PDVoltage5 > 3*maxV){
     writeReg(config_reg, 0b01001000);
     
-  // STEP 1&2: wait for at least 33 ms    //WILL: condense this by just calling "initADC()" function. 
-      uint8_t c = 1;
-  while (c) {
-    readRegs(busy_status_reg, 1, &c);
-    }
-    
-  // STEP 3: Choosing internal VREF and mode of operation
-  writeReg(advance_config_reg,advance_config_value);
-  readRegs(advance_config_reg,1, &advance_config_value);
-  Serial.println("Step3: operation mode");
-  Serial.println(advance_config_value);
-
-  // STEP 4: Enable Conversion Rate Register
-  writeReg(conv_rate_reg,conv_rate_value);  
-  readRegs(conv_rate_reg,1,&conv_rate_value);
-  Serial.println("Step 4b: Conversion Rate value:");
-  Serial.println(conv_rate_value);
-  
-  //STEP 5: Disabling unused inputs
-  writeReg(config_reg,config_default_value); // Disabling pins in shutdown mode
-  readRegs(config_reg,1,&test);
-  writeReg(disable_reg,disable_value);
-  readRegs(disable_reg,1,&disable_value);
-  Serial.println("Step 5a: Shut down first");
-  Serial.println(test,BIN);
-  Serial.println("Step5b: Disabling inputs");
-  Serial.println(disable_value,BIN);
-
-  //STEP 6: Enabling interrupt mask (won't go to INT pin)
-  writeReg(config_reg,0b0000001); // Powering ADC back on 
-  writeReg(interrupt_mask_reg,interrupt_mask_value);
-  readRegs(interrupt_mask_reg,1,&interrupt_mask_value);
-  Serial.println("Enabling interrupt mask");
-  Serial.println(interrupt_mask_value);
-  
-  //STEP7: Sending Limits for only enable input
-    writeReg(in_high_reg[0],in_high_val);
-    writeReg(in_low_reg[0],in_low_val);
-    readRegs(in_high_reg[0],1,&in_high_val);
-    readRegs(in_low_reg[0],1,&in_low_val);
-    Serial.println("Step7: Limits for IN0");
-    Serial.println(in_high_val);
-    Serial.println(in_low_val);
-    in_high_val = 0b101;
-    in_low_val = 0;
-    writeReg(in_high_reg[1],in_high_val);
-    writeReg(in_low_reg[1],in_low_val);
-    readRegs(in_high_reg[1],1,&in_high_val);
-    readRegs(in_low_reg[1],1,&in_low_val);
-    Serial.println("Step7: Limits for IN1");
-    Serial.println(in_high_val);
-    Serial.println(in_low_val);
-    in_high_val = 0b101;
-    in_low_val = 0;
-    writeReg(in_high_reg[2],in_high_val);
-    writeReg(in_low_reg[2],in_low_val);
-    readRegs(in_high_reg[2],1,&in_high_val);
-    readRegs(in_low_reg[2],1,&in_low_val);
-    Serial.println("Step7: Limits for IN2");
-    Serial.println(in_high_val);
-    Serial.println(in_low_val);
-    in_high_val = 0b101;
-    in_low_val = 0;
-    writeReg(in_high_reg[3],in_high_val);
-    writeReg(in_low_reg[3],in_low_val);
-    readRegs(in_high_reg[3],1,&in_high_val);
-    readRegs(in_low_reg[3],1,&in_low_val);
-    Serial.println("Step7: Limits for IN3");
-    Serial.println(in_high_val);
-    Serial.println(in_low_val);
-    in_high_val = 0b101;
-    in_low_val = 0;
-    writeReg(in_high_reg[4],in_high_val);
-    writeReg(in_low_reg[4],in_low_val);
-    readRegs(in_high_reg[4],1,&in_high_val);
-    readRegs(in_low_reg[4],1,&in_low_val);
-    Serial.println("Step7: Limits for IN4");
-    Serial.println(in_high_val);
-    Serial.println(in_low_val);    
-    
-  //STEP 8: Setting Start bit to 1   
-  writeReg(config_reg, start_value);
-  readRegs(config_reg, 1, &c);
-  Serial.println("Setting Start bit to 1");
-  Serial.println(c,BIN);
-
-  //STEP 9: Set interrupt to zero
-  writeReg(config_reg,int_clear_val);
-  readRegs(config_reg,1,&int_clear_val);
-  Serial.println("Setting interrupt to zero");
-  Serial.println(int_clear_val,BIN);
-  Serial.println("------------------------------------------------------------------------------------"); 
+  initADC();
   }
 }
 
@@ -282,9 +198,9 @@ void health()
 void getVoltage()
 {
   j = 0;
-  for(i=0; i<sizeof(PD_reg); i++){ 
-    Wire.beginTransmission(ADC_ser_address);            //WILL: can you just use your "readRegs()" function for this?
-    Wire.write(PD_reg[i]);     // Write register address
+  for(i=0; i<sizeof(PD_reg_array); i++){ 
+    Wire.beginTransmission(ADC_ser_address);            
+    Wire.write(PD_reg_array[i]);     // Write register address
     Wire.endTransmission(false);
     Wire.requestFrom(ADC_ser_address,uint8_t(2));
     while(Wire.available()){
