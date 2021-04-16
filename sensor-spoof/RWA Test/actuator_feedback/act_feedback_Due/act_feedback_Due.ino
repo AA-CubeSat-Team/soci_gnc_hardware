4646e03f7637eff46f0a3b9683876e0817ace288
hima1234 <48106767+william-pope@users.noreply.github.com>
2021-04-16 16:43:01 -0700

    16 Apr actuator feedback - desktop

--- sensor-spoof/RWA Test/actuator_feedback/act_feedback_Due/act_feedback_Due.ino
+++ sensor-spoof/RWA Test/actuator_feedback/act_feedback_Due/act_feedback_Due.ino
@@ -7,64 +7,38 @@
 #define ACT_DUE_ADDRESS 0x18
 #define DOUBLE_SIZE 8
 
+uint8_t rwa_currSpeed_bytes[4*DOUBLE_SIZE] = {0};
+uint8_t mtq_dipole_bytes[3*DOUBLE_SIZE] = {0};
+uint8_t act_feedback_bytes[7*DOUBLE_SIZE] = {0};
+
 void setup(){
   Serial.begin(115200);
   
   Wire.begin(ACT_DUE_ADDRESS);
   Wire.onReceive(receiveEvent);
-  
-  Serial.println("--- --- slave setup complete --- ---");
-  delay(100);
 }
 
 void loop(){
-  // UART transmit function call (need to turn off interrupts?)
+  memcpy(&act_feedback_bytes[0],&rwa_currSpeed_bytes[0],4*DOUBLE_SIZE);
+  memcpy(&act_feedback_bytes[0]+4*DOUBLE_SIZE,&mtq_dipole_bytes[0],3*DOUBLE_SIZE);
   
-  delay(100);
-
-//  if (rcv < DOUBLE_SIZE){
-//      rw1_currSpeed_bytes[rw1_ct] = sda_byte;
-//      rw1_ct++;
-//    }
-//    
-//    if (rcv >= DOUBLE_SIZE && rcv < 2*DOUBLE_SIZE){
-//      rw2_currSpeed_bytes[rw2_ct] = sda_byte;
-//      rw2_ct++;
-//    }
-//
-////    if (rcv >= 2*DOUBLE_SIZE && rcv < 3*DOUBLE_SIZE){
-////      rw3_currSpeed_bytes[rw3_ct] = sda_byte;
-////      rw3_ct++;
-////    }
+  Serial.write(&act_feedback_bytes[0],7*DOUBLE_SIZE);
+          
+  delay(1000);
 }
 
 void receiveEvent(int howMany){
-  uint8_t act_feedback_bytes[7*DOUBLE_SIZE] = {0};
-  
   int rcv = 0;
-  while(Wire.available() > 0){
-    act_feedback_bytes[rcv] = Wire.read(); 
-    rcv++;
+  if (howMany == 4*DOUBLE_SIZE){
+    while(Wire.available() > 0){
+      rwa_currSpeed_bytes[rcv] = Wire.read(); 
+      rcv++;
+    }
   }
-
-//          Serial.print("rw1_currSpeed_bytes:\t");
-//          for(int ii=0; ii<DOUBLE_SIZE; ii++){
-//            Serial.print(rw1_currSpeed_bytes[ii],HEX);
-//            Serial.print(" ");
-//          }
-//          Serial.println(" ");
-//        
-//          Serial.print("rw2_currSpeed_bytes:\t");
-//          for(int ii=0; ii<DOUBLE_SIZE; ii++){
-//            Serial.print(rw2_currSpeed_bytes[ii],HEX);
-//            Serial.print(" ");
-//          }
-//          Serial.println(" ");
-//
-//          Serial.print("rw3_currSpeed_bytes:\t");
-//          for(int ii=0; ii<DOUBLE_SIZE; ii++){
-//            Serial.print(rw3_currSpeed_bytes[ii],HEX);
-//            Serial.print(" ");
-//          }
-//          Serial.println(" ");
+  if (howMany == 3*DOUBLE_SIZE){
+    while(Wire.available() > 0){
+      mtq_dipole_bytes[rcv] = Wire.read(); 
+      rcv++;
+    }
+  }
 }
